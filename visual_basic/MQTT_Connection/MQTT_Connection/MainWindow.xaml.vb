379c7fdff3134d776170a12a0c60f5fd66aa19bf
Miguel Gonzalez <MGonzalez506@users.noreply.github.com>
2023-11-28 10:14:34 -0600

    Agregar soporte para recibir mensajes desde VB

--- visual_basic/MQTT_Connection/MQTT_Connection/MainWindow.xaml.vb
+++ visual_basic/MQTT_Connection/MQTT_Connection/MainWindow.xaml.vb
@@ -28,14 +28,58 @@ Class MainWindow
 
     Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs)
 
-        'Get project main parent path
-        Dim path As String = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName
-        Console.WriteLine(path)
+        Try
+            'Get project main parent path
+            Dim path As String = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName
+            'Read .env and parse it as json
+            Dim json As JObject = JObject.Parse(File.ReadAllText(path + "\.env"))
+            Console.WriteLine(path)
+
+            MQTT_CLIENT_ID = json.SelectToken("Cliente_ID").ToString()
+            MQTT_USERNAME = json.SelectToken("Cliente_Username").ToString()
+            MQTT_PASSWORD = json.SelectToken("Cliente_Password").ToString()
+            MQTT_SERVER_URL = json.SelectToken("MQTT_SERVER_HOST").ToString()
+            MQTT_SERVER_PORT = json.SelectToken("MQTT_SERVER_PORT")
+            MQTT_TOPIC_TO_SEND = json.SelectToken("TEMA_PARA_ENVIO").ToString()
+            MQTT_TOPIC_TO_RECIEVE = json.SelectToken("TEMA_PARA_RECEPCION").ToString()
+
+            Console.WriteLine("Cliente_ID: " & MQTT_CLIENT_ID)
+            Console.WriteLine("Cliente_Username: " & MQTT_USERNAME)
+            Console.WriteLine("Cliente_Password: " & MQTT_PASSWORD)
+            Console.WriteLine("MQTT_SERVER_HOST: " & MQTT_SERVER_URL)
+            Console.WriteLine("MQTT_SERVER_PORT: " & MQTT_SERVER_PORT.ToString())
+            Console.WriteLine("TEMA_PARA_ENVIO: " & MQTT_TOPIC_TO_SEND)
+            Console.WriteLine("TEMA_PARA_RECEPCION: " & MQTT_TOPIC_TO_RECIEVE)
 
 
+        Catch ex As Exception
+            MessageBox.Show("Error al leer el archivo con las credenciales, saliendo del programa...")
+            Application.Current.Shutdown()
+        End Try
 
+        client = New MqttClient(MQTT_SERVER_URL, MQTT_SERVER_PORT, False, Nothing, Nothing, MqttSslProtocols.None)
 
+        Try
+            client.Connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)
+            Thread.Sleep(2000)
 
+            If client.IsConnected Then
+                client.Subscribe(New String() {MQTT_TOPIC_TO_RECIEVE}, New Byte() {MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE})
+            Else
+                MessageBox.Show("Error al conectarse al servidor MQTT, saliendo del programa...")
+                Application.Current.Shutdown()
+            End If
+            AddHandler client.MqttMsgPublishReceived, AddressOf MessageReceived
+        Catch ex As Exception
+            MessageBox.Show("Error al conectarse al servidor MQTT, saliendo del programa...")
+            Application.Current.Shutdown()
+        End Try
+
+    End Sub
+
+    Private Sub MessageReceived(sender As Object, e As MqttMsgPublishEventArgs)
+        Dim message As String = System.Text.Encoding.UTF8.GetString(e.Message)
+        Console.WriteLine("Mensaje recibido: " & message)
     End Sub
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
 
