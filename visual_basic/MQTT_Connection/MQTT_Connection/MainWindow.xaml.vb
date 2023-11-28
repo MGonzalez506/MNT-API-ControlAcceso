18995f2e8aa865e18dcbed441d8b959c453ab0e8
Miguel Gonzalez <MGonzalez506@users.noreply.github.com>
2023-11-28 07:11:12 -0600

    Cargando código inicial

---
+++ visual_basic/MQTT_Connection/MQTT_Connection/MainWindow.xaml.vb
@@ -0,0 +1,26 @@
+﻿Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Windows
+Imports System.Threading
+Imports System.Diagnostics.Eventing.Reader
+
+' --- MQTT Imports ---
+Imports uPLibrary.Networking.M2Mqtt
+Imports uPLibrary.Networking.M2Mqtt.Messages
+Imports uPLibrary.Networking.M2Mqtt.Exceptions
+
+' --- JSON Imports
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+
+Class MainWindow
+    Inherits Window
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
