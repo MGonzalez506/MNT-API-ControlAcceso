ea0701505dc1fadb71aabf1dc1fd3ea423c25f06
MGonzalez506 <dev@mgonzalez506.com>
2026-02-19 21:08:13 -0600

    Actualizar para conectarse a la versión nueva de API de Mau

--- python/terminal_menu.py
+++ python/terminal_menu.py
@@ -14,10 +14,14 @@ import paho.mqtt.client as mqtt
 from dotenv import load_dotenv
 from json_texts import get_json_example
 
-load_dotenv(override=True)
+# Ensure the .env file is loaded from the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(current_dir, '.env')
+load_dotenv(dotenv_path, override=True)
 
 # Credenciales MQTT
-MQTT_CERT_FILE_PATH = os.getenv('MQTT_CERT_FILE_PATH')
+MQTT_USE_TLS = os.getenv('MQTT_USE_TLS')
+MQTT_CERT_FILE_PATH = os.path.join(current_dir, os.getenv('MQTT_CA_FILE'))
 MQTT_CLIENT_ID = os.getenv('Cliente_ID')
 MQTT_USERNAME = os.getenv('Cliente_Username')
 MQTT_PASSWORD = os.getenv('Cliente_Password')
@@ -25,6 +29,8 @@ MQTT_SERVERURL = os.getenv('MQTT_SERVER_HOST')
 MQTT_SERVERPORT = int(os.getenv('MQTT_SERVER_PORT'))
 MQTT_TOPIC_TO_SEND = os.getenv('MQTT_TOPIC_TO_SEND')
 MQTT_TOPIC_TO_RECEIVE = os.getenv('MQTT_TOPIC_TO_RECEIVE')
+MQTT_QOS = int(os.getenv('MQTT_QOS', 2))
+MQTT_KEEPALIVE = int(os.getenv('MQTT_KEEPALIVE', 60))
 
 contador_global_de_respuestas = 0
 
@@ -33,7 +39,7 @@ def send_to_mqtts(topic, msg, print_topic_and_msg):
 		os.write(sys.stdout.fileno(), topic.encode('utf-8'))
 		os.write(sys.stdout.fileno(), msg.encode('utf-8'))
 		os.write(sys.stdout.fileno(), "\n".encode('utf-8'))
-	mqtt_client.publish(MQTT_TOPIC_TO_SEND, msg, qos=2, retain=False)
+	mqtt_client.publish(MQTT_TOPIC_TO_SEND, msg, qos=MQTT_QOS, retain=False)
 
 def on_connect(client, userdata, flags, rc):
 	print("Connected to broker")
@@ -59,7 +65,25 @@ def on_message(client, userdata, msg):
 		# Print JSON Beautified
 		contador_global_de_respuestas += 1
 		respuesta_a_imprimir = "Respuesta : " + str(contador_global_de_respuestas) + " - " + mqtt_msg
-		print(respuesta_a_imprimir)
+		# Respuesta a imprimir is a json string, check the property "module" and confirm is not "actualizar_nivel_de_acceso" or "eliminar_usuario", if it is one of those, print the message without beautifying, if not, print the message beautified
+		if "actualizar_nivel_de_acceso" in mqtt_msg or "eliminar_usuario" in mqtt_msg or "obtener_mnt_id_persona" in mqtt_msg:
+			respuesta_a_imprimir = "Respuesta : " + str(contador_global_de_respuestas) + " - " + mqtt_msg
+			print(respuesta_a_imprimir)
+		else:
+			respuesta_a_imprimir = "Respuesta : " + str(contador_global_de_respuestas) + " - " + json.dumps(json.loads(mqtt_msg), indent=4, sort_keys=True)
+			print("\n\n\n")
+			print(respuesta_a_imprimir)
+			print("\n\n\n")
+			# Check if mnt_id_persona exists in the message, if it does, send to "actualizar_nivel_de_acceso" topic a message with the mnt_id_persona and the new access level "nivel_de_acceso": 2
+			if "mnt_id_persona" in mqtt_msg:
+				json_msg = json.loads(mqtt_msg)
+				mnt_id_persona = json_msg['body']['datos']['mnt_id_persona']
+				json_to_send = get_json_example('actualizar_nivel_de_acceso')
+				json_to_send = json.loads(json_to_send)
+				json_to_send['body']['mnt_id_persona'] = mnt_id_persona
+				json_to_send = json.dumps(json_to_send)
+				print("\n\n\nSe ha detectado el mnt_id_persona en la respuesta, se enviará un mensaje para actualizar el nivel de acceso a 2\n\n\n")
+				send_to_mqtts(MQTT_TOPIC_TO_SEND, json_to_send, False)
 		#print(json.dumps(json_msg, indent=4, sort_keys=True))
 	except:
 		print("\n\n\nError al convertir el mensaje recibido a JSON\n\n\n")
@@ -71,12 +95,18 @@ def on_message(client, userdata, msg):
 
 mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
 mqtt_client.on_log = on_log
-#mqtt_client.tls_set(MQTT_CERT_FILE_PATH)
+# Check if the certificate file exists before setting TLS
+if MQTT_USE_TLS.lower() == "true":
+    if os.path.isfile(MQTT_CERT_FILE_PATH):
+        mqtt_client.tls_set(MQTT_CERT_FILE_PATH)
+    else:
+        print(f"Error: Certificate file not found at {MQTT_CERT_FILE_PATH}")
+        exit(1)
 mqtt_client.on_connect = on_connect
 mqtt_client.on_disconnect = on_disconnect
 mqtt_client.on_message = on_message
 mqtt_client.username_pw_set(MQTT_USERNAME, password=MQTT_PASSWORD)
-mqtt_client.connect(MQTT_SERVERURL, MQTT_SERVERPORT, keepalive=15)
+mqtt_client.connect(MQTT_SERVERURL, MQTT_SERVERPORT, keepalive=MQTT_KEEPALIVE)
 
 if __name__ == "__main__":
 	mqtt_client.loop_start()
@@ -88,15 +118,14 @@ if __name__ == "__main__":
 	menu += "Topic to send: " + MQTT_TOPIC_TO_SEND + "\n"
 	menu += "Topic to receive: " + MQTT_TOPIC_TO_RECEIVE + "\n"
 	menu += "---------------------------------------------\n"
-	menu += "1. Agregar un usuario con fotografia (Toma unos 5 - 7 segundos en responder)\n"
-	menu += "2. Agregar sin fotografía (Toma unos 2.4 segundos)\n"
-	menu += "3. Aplicar nivel de acceso\n"
-	menu += "4. Obtener información de la persona con su mnt_id_persona\n"
-	menu += "5. Editar un usuario (para editar la fotografía utiliza la opción 6)\n"
-	menu += "6. Editar un rostro (Toma unos 5 - 7 segundos en responder)\n"
-	menu += "7. Eliminar un usuario\n"
-	menu += "8. Obtener el mnt_id_persona a partir del persona_id\n"
-	menu += "9. Agregar varios usuarios\n"
+	menu += "1. Agregar sin fotografía (Toma unos 2.4 segundos)\n"
+	menu += "2. Aplicar nivel de acceso\n"
+	menu += "3. Obtener información de la persona con su mnt_id_persona\n"
+	menu += "4. Editar un usuario (para editar la fotografía utiliza la opción 6)\n"
+	menu += "5. Editar un rostro (Toma unos 5 - 7 segundos en responder)\n"
+	menu += "6. Eliminar un usuario\n"
+	menu += "7. Obtener el mnt_id_persona a partir del persona_id\n"
+	menu += "8. Agregar varios usuarios\n"
 	menu += "s. Salir...\n"
 	print(menu)
 
@@ -108,20 +137,18 @@ if __name__ == "__main__":
 		if input_text == '1':
 			send_to_mqtts(MQTT_TOPIC_TO_SEND, get_json_example('agregar_usuario'), False)
 		elif input_text == '2':
-			send_to_mqtts(MQTT_TOPIC_TO_SEND, get_json_example('agregar_usuario_sin_foto'), False)
-		elif input_text == '3':
 			send_to_mqtts(MQTT_TOPIC_TO_SEND, get_json_example('actualizar_nivel_de_acceso'), False)
-		elif input_text == '4':
+		elif input_text == '3':
 			send_to_mqtts(MQTT_TOPIC_TO_SEND, get_json_example('obtener_informacion_de_persona_por_id'), False)
-		elif input_text == '5':
+		elif input_text == '4':
 			send_to_mqtts(MQTT_TOPIC_TO_SEND, get_json_example('editar_usuario'), False)
-		elif input_text == '6':
+		elif input_text == '5':
 			send_to_mqtts(MQTT_TOPIC_TO_SEND, get_json_example('editar_rostro'), False)
-		elif input_text == '7':
+		elif input_text == '6':
 			send_to_mqtts(MQTT_TOPIC_TO_SEND, get_json_example('eliminar_usuario'), False)
-		elif input_text == '8':
+		elif input_text == '7':
 			send_to_mqtts(MQTT_TOPIC_TO_SEND, get_json_example('obtener_mnt_id_persona'), False)
-		elif input_text == '9':
+		elif input_text == '8':
 			# Digita la cantidad de usuarios que se desean simular
 			cantidad_de_usuarios = input("Ingrese la cantidad de usuarios a simular: ")
 			# Genera un conjunto de cuatro letras aleatorias
